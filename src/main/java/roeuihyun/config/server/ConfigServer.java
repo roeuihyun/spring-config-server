023f9c59b877a41fcc2ddae2ce9250126645dcd2
roeuihyun <roeuihyun@gmail.com>
2021-07-15 14:33:11 +0900

    application.yml add & configserver annotation add

--- src/main/java/roeuihyun/config/server/ConfigServer.java
+++ src/main/java/roeuihyun/config/server/ConfigServer.java
@@ -2,8 +2,10 @@ package roeuihyun.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServer {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer.class, args);
